781f68ce20c94dede1090a05c82ed67e401ce1ec
placards <miazpras@gmail.com>
2019-03-30 16:19:26 +0300

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,23 @@
+var darkTheme;
 var switchToDark = function () {
-    alert("Hello!");
+    if (darkTheme == true) {
+        document.getElementById("title").className = "title";
+        document.getElementById("snowflake").className = "snowflakek";
+        document.getElementById("text").className = "text";
+        document.body.classList.remove("dark");
+        document.getElementById("footer").className = "footer";
+        document.getElementById("darkLight").innerHTML = "Dark Mode";
+        document.getElementById("darkLight").className = "darkLight";
+        darkTheme = false;
+    } else {
+        document.getElementById("title").className = "title dark";
+        document.getElementById("snowflake").className = "snowflake dark";
+        document.getElementById("text").className = "text dark";
+        document.body.classList.toggle("dark");
+        document.getElementById("footer").className = "footer dark";
+        document.getElementById("darkLight").innerHTML = "Light Mode";
+        document.getElementById("darkLight").className = "darkLight dark";
+        darkTheme = true;
+    }
+
 }
